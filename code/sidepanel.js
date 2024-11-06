be06b08cf193a6a45b670e3fe98e931f43a1da63
Edwin Toh <edwin@runwayml.com>
2024-11-06 15:56:12 -0500

    update X icon

--- code/sidepanel.js
+++ code/sidepanel.js
@@ -145,7 +145,10 @@ function renderVideo(videoUrl) {
   const removeButton = document.createElement('button');
   removeButton.className = 'remove-button';
   removeButton.setAttribute('data-url', videoUrl);
-  removeButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-x"><path d="M18 6 6 18"/><path d="m6 6 12 12"/></svg>`;
+  removeButton.innerHTML = `<svg width="19" height="19" viewBox="0 0 19 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M17.97 17.26L10.19 9.49L17.97 1.71L17.26 1L9.49 8.78L1.71 1L1 1.71L8.78 9.49L1 17.26L1.71 17.97L9.49 10.19L17.26 17.97L17.97 17.26Z" fill="#0C0C0C"/>
+</svg>
+`;
   videoWrapper.appendChild(removeButton);
 
   // Add event listener to the new remove button
